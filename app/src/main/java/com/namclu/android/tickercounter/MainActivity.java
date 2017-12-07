5d994701395d5d09e03c97b6cc4f08aae6104fff
namclu <namlu1205@gmail.com>
2017-12-07 14:52:28 +1100

    1.00 feat: implement +1 and RESET button behaviour

--- app/src/main/java/com/namclu/android/tickercounter/MainActivity.java
+++ app/src/main/java/com/namclu/android/tickercounter/MainActivity.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private int mCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        private Button addOneButton;
-        private Button resetCountButton;
+        Button addOneButton;
+        Button resetCountButton;
 
         addOneButton = findViewById(R.id.button_add_one_count);
         addOneButton.setOnClickListener(this);
@@ -25,7 +28,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
+            case R.id.button_add_one_count:
+                mCounter++;
+                updateCounterView();
+                break;
+            case R.id.button_reset_count:
+                mCounter = 0;
+                updateCounterView();
+                break;
         }
+    }
+
+    private void updateCounterView() {
+        TextView counterView = findViewById(R.id.text_counter);
+        counterView.setText(String.valueOf(mCounter));
     }
 }
